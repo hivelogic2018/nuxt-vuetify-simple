84bbca7b42e44fdd9d5842a6cdb70e0fb7ae4ad4
Kim Tran <mykimgiacmo@gmail.com>
2025-06-18 14:01:50 -0500

    updated

--- pages/camnang/index.vue
+++ pages/camnang/index.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue'
 import { useCookie } from '#app'
-import MarkdownRenderer from '~/components/MarkdownRenderer.vue'
 import camnangMd from '~/content/index.md?raw'
 
 const tocCookie = useCookie('showToc', { default: () => 'false' })
