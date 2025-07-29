1965ea1a58910ca71b2f2c012d3e8dc8ac49945f
Tracy <tracytrinh@goldenwestconsulting.com>
2025-07-29 09:27:21 -0700

    fix imports

--- pages/jsonschema/[slug].vue
+++ pages/jsonschema/[slug].vue
@@ -2,8 +2,8 @@
 import { ref, watchEffect } from 'vue'
 import { useRoute } from 'vue-router'
 
-import DynamicPage from '../../components/DynamicPage.vue'
-import type { PageSchema } from '../../types/page'
+import DynamicPage from '@/components/DynamicPage.vue'
+import type { PageSchema } from '@/types/page'
 
 const route = useRoute()
 const schema = ref<PageSchema | null>(null)
@@ -11,7 +11,7 @@ const slug = ref(route.params.slug as string)
 
 watchEffect(async () => {
 	try {
-		const pageSchema = await import(`../../schemas/service-debugger.json`)
+		const pageSchema = await import(`@/schemas/service-debugger.json`)
 		schema.value = pageSchema as PageSchema
 	} catch (e) {
 		console.error(`Failed to load schema for: ${slug.value}`, e)
